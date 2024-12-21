b50ec85ee03a5f8f9503204297f279729886b0c4
omarbinsaleh <omarbinsaleh44@gmail.com>
2024-12-22 00:06:22 +0400

    done the basic setup and integrated the mongoDB and deployed to using vercel

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+// create the express application:
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares:
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fev0e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+async function run() {
+   try {
+
+      app.get('/', (req, res) => {
+         res.send('Serever is running....')
+      })
+
+
+      // Connect the client to the server	(optional starting in v4.7)
+      // await client.connect();
+      // Send a ping to confirm a successful connection
+      // await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      // await client.close();
+   }
+}
+run().catch(console.dir);
+
+
+
+// listen the sever:
+app.listen(port, () => {
+   console.log(`Marathon Hub Server is running on port: ${port}`);
+})
